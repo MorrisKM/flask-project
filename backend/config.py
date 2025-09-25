ae223f15f5856498c7ef31d20a80c0b99a7355fb
Morris <itsmorriskiburi@gmail.com>
2025-09-25 10:02:42 +0300

    deployment setups

--- backend/config.py
+++ backend/config.py
@@ -4,11 +4,16 @@ import os
 #to load data inside the dotenv call load_dotenv
 load_dotenv()
 class Config:
-  SECRET_KEY = os.getenv('SECRET_KEY')
+  SECRET_KEY = os.getenv('SECRET_KEY', "secret")
   JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
 class DevConfig:
   SQLALCHEMY_DATABASE_URI = "sqlite:///events.db"
   SQLALCHEMY_ECHO=True
   DEBUG=True
+
+class ProdConfig(Config):
+  SQLALCHEMY_DATABASE_URI="sqlite:///prod.db"
+  DEBUG=os.getenv("DEBUG", False)
+  SQLALCHEMY_ECHO=os.getenv('ECHO', False)
